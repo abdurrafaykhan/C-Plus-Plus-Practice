c15a732e9d146e155ead0a4f66d4739e9ea7bcb1
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-05-30 22:19:08 -0400

    Commented code

--- LeetCode/TwoSum.cpp
+++ LeetCode/TwoSum.cpp
@@ -2,24 +2,31 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
+        // Initializes final vector to be returned
         vector<int> final;
+
+        // Goes through all of the indexes for the first number
         for (int i = 0; i < nums.size(); i++) {
             
+            // Goes through all of the indexes for the second number
             for (int j = 0; j < nums.size(); j++) {
                 
+                // If the indexes are not the same
                 if (i != j) {
+
+                    // Check if they add up to the target
+                    // If they do, add to the final vectore and return
                     if ((nums[i] + nums[j]) == target) {
                     
-                    final.push_back(i);
-                    final.push_back(j);
-                    return final;
+                        final.push_back(i);
+                        final.push_back(j);
+                        return final;
                     }
                 }
             }
         }
         
-        
+        // If target is not achieved as a sum, return empty vector
         return final;
-        
     }
 };
