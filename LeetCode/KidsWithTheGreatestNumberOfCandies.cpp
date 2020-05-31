a533ac025eee91318b44969c90a4eaf4ecc52f29
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-05-31 05:30:08 -0400

    Improved comments and organization of code

--- LeetCode/KidsWithTheGreatestNumberOfCandies.cpp
+++ LeetCode/KidsWithTheGreatestNumberOfCandies.cpp
@@ -2,28 +2,32 @@ class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
         
+        // Iniitalizes final vector to be returned
         vector<bool> final;
         
-        
+        // Finds the maximum number in the original vector
         int max = *max_element(candies.begin(), candies.end());
         
+        // Sets a flag to determine with kid has the greatest number of candies
         bool flag = true;
         
+        // Goes through all indexes
         for (int i = 0; i < candies.size(); i++) {
             
+            // Resets flag after every kid
             flag = true;
             
-            if ((candies[i] + extraCandies) < max) {
-            
+            // Checks if i'th kid with extraCandies has less than the max
+            // Sets flag to false accordingly
+            if ((candies[i] + extraCandies) < max) {      
                 flag = false;
             }
             
+            // Pushes back flag for the i'th kid
             final.push_back(flag);
-            
-            
-            
         }
         
+        // Returns final vector
         return final;
         
         
