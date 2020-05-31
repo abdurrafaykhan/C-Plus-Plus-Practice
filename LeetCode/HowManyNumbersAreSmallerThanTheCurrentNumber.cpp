0b9643ffe638f790bb691981e0b833b4d55c4c55
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-05-30 22:37:27 -0400

    Commented code

--- LeetCode/HowManyNumbersAreSmallerThanTheCurrentNumber.cpp
+++ LeetCode/HowManyNumbersAreSmallerThanTheCurrentNumber.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
         
-    
+        // Initialized vector to be returned
         vector<int> final;
         
+        // Goes through all indexes
         for (int i = 0; i < nums.size(); i++) {
             
+            // Initialized variable to count how many numbers i is bigger than
             int sum = 0;
             
+            // Compares i with all j indexes and increments sum accordingly
             for (int j = 0; j < nums.size(); j++) {
                 
                 if (nums[i] > nums[j]) {
@@ -16,10 +19,11 @@ public:
                 }
             }
             
+            // Adds the sum to the final vector 
             final.push_back(sum);
-            
         }
         
+        // Returns final array
         return final;
 
         
