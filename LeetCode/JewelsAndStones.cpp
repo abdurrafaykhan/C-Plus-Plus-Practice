f1507d910d4dd83fbfa0ce62c44e692d61fe4f34
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-06-01 06:23:10 -0400

    Commented and Organized code

--- LeetCode/JewelsAndStones.cpp
+++ LeetCode/JewelsAndStones.cpp
@@ -2,30 +2,20 @@ class Solution {
 public:
     int numJewelsInStones(string J, string S) {
         
-        
+        // Initialized counter to be returned
         int count = 0;
         
-        
+        // Compare every character in J with every character in S
+        // If the characters match, increase count
         for (int i = 0; i < J.size(); i++) {
-            
             for (int j = 0; j < S.size(); j++) {
-                
                 if (J[i] == S[j]) {
                     count = count + 1;
                 }  
             }
         }
         
-        return count;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        // Return count
+        return count;  
     }
 };
