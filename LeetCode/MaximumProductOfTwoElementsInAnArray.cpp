64ed9b90f4d71c90018b9a72f3abb0156079ecdd
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-06-01 08:40:40 -0400

    Improved efficiency by 20%

--- LeetCode/MaximumProductOfTwoElementsInAnArray.cpp
+++ LeetCode/MaximumProductOfTwoElementsInAnArray.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         
-        
+        // Calculates size of the array
         int size = nums.size();
         
+        // Sorts the list of numbers
         sort(nums.begin(), nums.end());
         
+        // Sets the numbers for the product to be the last two elements of the list
         int num1 = nums[size-1] - 1;
         int num2 = nums[size-2] - 1;
+
+        // Calculates product
         int product = num1 * num2;
         
+        // Returns product
         return product;
-        
     }
 };
