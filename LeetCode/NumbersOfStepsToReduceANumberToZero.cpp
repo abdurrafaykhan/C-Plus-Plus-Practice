dee11a57f5c56cf7e32f606dc88eb81350af2285
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-06-01 04:22:05 -0400

    Organized and commented code

--- LeetCode/NumbersOfStepsToReduceANumberToZero.cpp
+++ LeetCode/NumbersOfStepsToReduceANumberToZero.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     int numberOfSteps (int num) {
         
+        // Initialize count to 0 
         int count = 0;
         
+        // While its not 0
         while (num != 0) {
             
+            // If its an even number, divide by 0
             if (num % 2 == 0) {
                 num = num/2;
             }
+
+            // If an odd number, subtract 1
             else {
                 num = num -1;
             }
+            
+            // Increase the count by 1 for each step
             count = count + 1;
         }
-        return count;
-        
-        
-        
-        
+
+        // Returns total count
+        return count;    
     }
 };
