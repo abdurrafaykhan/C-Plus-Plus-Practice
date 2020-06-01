a4db7ee376bf72fe880e9b9b9c0fbda1ef21376a
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-06-01 07:03:48 -0400

    Organized code and commented

--- LeetCode/MakeTwoArraysEqualByReversingSubArrays.cpp
+++ LeetCode/MakeTwoArraysEqualByReversingSubArrays.cpp
@@ -2,19 +2,26 @@ class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
         
+        // Initialized variable that determines if arr is reversible to target
         bool reversable = false;
         
+        // If the two arrays are the same, so it is reversible
         if (target == arr) {
             reversable = true;
         }
+
+        // If the sizes of the arrays are different, arr can not reverse in any way to match target
+        // So it is not reversable
         else if (target.size() != arr.size()){
             reversable = false;
         }
+
+        // If the sizes are the same, sort both from least to greatest, and if they're equal, it is reversable
+        // If not equal, not reversible
         else {
-            
             sort(target.begin(), target.end());
             sort(arr.begin(), arr.end());
-            
+
             if (target == arr) {
                 reversable = true;
             }
@@ -23,7 +30,7 @@ public:
             } 
         }
         
-        
+        // Return status of reversability
         return reversable;
   
     }
