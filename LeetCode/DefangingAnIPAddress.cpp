77f14490bf12f26ec0fee51aa8b9670f1d1dfe8b
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-06-01 04:00:18 -0400

    Commented and organized code

--- LeetCode/DefangingAnIPAddress.cpp
+++ LeetCode/DefangingAnIPAddress.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     string defangIPaddr(string address) {
         
+        // Initializes final string to be returned
         string final;
         
+        // Goes through every character in address
         for (int i = 0; i < address.size(); i++) {
             
+            // If there is a '.', replace '[.]'
+            // If not, keep the same character from address
             if (address[i] != '.') {
                 final = final + address[i];
             }
             else {
                 final = final + "[.]";
             }
-            
-            
-            
         }
         
+        // Return final string
         return final;
-        
-        
-        
     }
 };
