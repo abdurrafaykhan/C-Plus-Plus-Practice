d601973ffb7fb896a301e8a175460f5266e7e295
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-06-04 19:45:30 -0400

    Commented code

--- LeetCode/FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree.cpp
+++ LeetCode/FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree.cpp
@@ -11,26 +11,26 @@
 class Solution {
 public:
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-        
-        
-        
-        
+
+        // Return the target when its found
         if (original == target) {
             return cloned;
         }
+
+        // If the node is empty, return null
         else if (original == nullptr) {
             return nullptr;
         }
         
+        // Find the left child of the node
         TreeNode* found = getTargetCopy(original->left, cloned->left, target);
+
+        // If the left child is an actual node, return it
         if (found != nullptr) {
             return found; 
         }
-        
+
+        // Continue to look for target in right child
         return getTargetCopy(original->right, cloned->right, target);
-        
-        
-  
-        
     }
 };
