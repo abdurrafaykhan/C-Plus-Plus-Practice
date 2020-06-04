b73ff3c855ca0554c3eaa48eb39b3b8766a78c07
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-06-04 08:38:36 -0400

    Removed debugging statements

--- LeetCode/SumOfNodesWithEvenValuedGrandparent.cpp
+++ LeetCode/SumOfNodesWithEvenValuedGrandparent.cpp
@@ -19,14 +19,12 @@ public:
         
         // Base case when the node is empty
         if (root == nullptr) {
-            cout << "in base case" <<endl;
             return 0;
         }
 
         // When there is a node
         else {
             
-            cout << "before" <<endl;
             // Check if node is even number, if even...
             if ((root->val)%2 == 0) {
                 
@@ -35,16 +33,12 @@ public:
 
                     // Check if the node has a left-left grandchild, if grandchild exists, add to sum
                     if (root->left->left != nullptr) {
-                        cout << "in 1 case" << root->left->left->val << endl;
                         sum = sum + root->left->left->val;
-                        cout << "in 1 case" << sum << endl;
                     }
 
                     // Check if the node has a left-right grandchild, if grandchild exists, add to sum
                     if (root->left->right != nullptr) {
-                        cout << "in 2 case" << root->left->right->val << endl;
                         sum = sum + root->left->right->val;
-                        cout << "in 2 case" << sum << endl;
                     }
                 }
                 
@@ -53,20 +47,15 @@ public:
 
                     // Check if the node has a right-left grandchild, if grandchild exists, add to sum
                     if (root->right->left != nullptr) {
-                        cout << "in 3 case" << root->right->left->val << endl;
                         sum = sum + root->right->left->val;
-                        cout << "in 3 case" << sum << endl;
                     }
 
                     // Check if the node has a right-right grandchild, if grandchild exists, add to sum
                     if (root->right->right != nullptr) {
-                        cout << "in 4 case" << root->right->right->val << endl;
                         sum = sum + root->right->right->val;
-                        cout << "in 4 case" << sum << endl;
                     }
                }
-               
-                cout << "after" << sum << endl;
+
                 // Recursively add sum to nodes children
                 return sum + sumEvenGrandparent(root->left) + sumEvenGrandparent(root->right);
             }
