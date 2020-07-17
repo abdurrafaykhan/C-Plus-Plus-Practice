a39e98aedd9d307ef31d5265389f1d19f6a4cabb
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-07-17 02:54:59 -0400

    Completed problem counting number of sequential pairs in a vector

---
+++ LeetCode/NumberOfGoodPairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                
+                if (i == j) {
+                    
+                }
+                else if (i > j) {
+                    
+                }
+                else {
+                    if (nums[i] == nums[j]) {
+                        count = count + 1;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
