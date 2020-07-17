5cdf98bee1a07640061f06b886e4952324ed6015
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-07-17 02:58:30 -0400

    Commented code

--- LeetCode/NumberOfGoodPairs.cpp
+++ LeetCode/NumberOfGoodPairs.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        
+
+        // Initialize final count to be returned
         int count = 0;
+
+        // Goes through all of nums
         for (int i = 0; i < nums.size(); i++) {
+            
+            // Compares one num to another
             for (int j = 0; j < nums.size(); j++) {
-                
+
+                // if the indices are the same, do nothing
                 if (i == j) {
-                    
                 }
+
+                // If the first number comes after the second
                 else if (i > j) {
-                    
                 }
+
+                // Otherwise, if the numbers are the same, increment count
                 else {
                     if (nums[i] == nums[j]) {
                         count = count + 1;
@@ -19,6 +27,8 @@ public:
                 }
             }
         }
+
+        // Return count
         return count;
     }
 };
